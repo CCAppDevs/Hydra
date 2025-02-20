d9a9e0a13d09ca3454fe64a87053f9d9729e6dcd
Jesse Harlan <jesse.harlan@centralia.edu>
2025-02-20 12:06:04 -0800

    Initial

---
+++ Library/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Description { get; set; }
+        public EventType Type { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string HostAddress { get; set; }
+    }
+}
